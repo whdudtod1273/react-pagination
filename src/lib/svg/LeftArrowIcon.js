217f05ebb8882f02af75a7b5b858f2bc26ef1b9b
whdudtod1273 <whdudtod1273@naver.com>
2021-07-10 14:18:49 +0900

    delete svg file, create svg component

---
+++ src/lib/svg/LeftArrowIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const LeftArrowIcon = ({ width, height, stroke }) => {
+
+  return (
+    <svg width={width} height={height} viewBox="0 0 60 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path id="Vector" d="M5 54.9461L54.8923 5M5.10768 55.0539L55 105" stroke={stroke} stroke-width="10" stroke-linecap="round" />
+    </svg>
+
+  )
+}
+export default LeftArrowIcon
---
+++ src/lib/svg/RightArrowIcon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const RightArrowIcon = ({ width, height, stroke }) => {
+
+  return (
+    <svg width={width} height={height} viewBox="0 0 60 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path id="Vector" d="M55 55.0539L5.10769 105M54.8923 54.9461L5 5" stroke={stroke} stroke-width="10" stroke-linecap="round" />
+    </svg>
+  )
+}
+export default RightArrowIcon
